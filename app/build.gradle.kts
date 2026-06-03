1f0f5886fec5c0cb5acaeed328ce72461e2190ff
JesseBennett-lab <j.bennett0505@gmail.com>
2026-06-03 15:00:22 +0200

    finishing touches

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.generationsgenerator"
-        minSdk = 25
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/com/example/generationsgenerator/MainActivity.kt
+++ app/src/main/java/com/example/generationsgenerator/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.generationsgenerator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,8 +15,21 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val birthYearEditText = findViewById<EditText>(R.id.birthYearEditText)
+        val generateButton = findViewById<Button>(R.id.generateButton)
+        val displayTextView = findViewById<TextView>(R.id.displayTextView)
 
-
+        val year = birthYearEditText.text.toString().toInt()
+        val generation = when (year) {
+            in 1928..1945 -> "Silent Generation"
+            in 1946..1964 ->  "Baby Boomer"
+            in 1965..1980 -> "Gen X"
+            in 1981..1996 -> "Millennial"
+            in 1997..2012 -> "Gen Z"
+            in 2013..2024 -> "Gen Alfa"
+            else -> "Unknown generation"
+        }
+        displayTextView.text = "Your generation: $generation"
 
 
 
